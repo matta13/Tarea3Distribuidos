1b8d037a1bd1c6c4e237cac9acfa370285c46c5c
matta13 <martin.matta@mail.udp.cl>
2025-11-21 02:47:28 -0300

    Add yahoo_dataset and llm_dataset tables with imports

--- postgres/01schema.sql
+++ postgres/01schema.sql
@@ -6,3 +6,20 @@ CREATE TABLE IF NOT EXISTS public.querys (
   body   text,
   answer text
 );
+
+CREATE TABLE IF NOT EXISTS public.yahoo_dataset (
+    score TEXT,
+    title TEXT,
+    body TEXT,
+    answer TEXT
+);
+
+CREATE TABLE IF NOT EXISTS public.llm_dataset (
+    score TEXT,
+    title TEXT,
+    body TEXT,
+    answer TEXT
+);
+
+COPY public.yahoo_dataset FROM '/data_import/train_10k.csv' DELIMITER ',' CSV HEADER;
+COPY public.llm_dataset FROM '/data_import/train_10k_actualizado.csv' DELIMITER ',' CSV HEADER;
